0900a5224004ef6c1836de21cf6aa41d166af711
Maddie <madalina.khaji10@gmail.com>
2019-05-14 10:56:53 +0200

    Day 6 pre lunch

---
+++ GTL/GTL/Model/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTL.Model
+{
+    public class Card
+    {
+        public int CardNo { get; set; }
+        public string CardIssuedate { get; set; }
+    }
+}
