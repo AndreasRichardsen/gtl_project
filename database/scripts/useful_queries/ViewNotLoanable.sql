6f00ed418c64758bb743a891a3d65c8e5f8190fa
Maddie <madalina.khaji10@gmail.com>
2019-05-28 14:10:15 +0200

    Update report

--- database/scripts/useful_queries/ViewNotLoanable.sql
+++ database/scripts/useful_queries/ViewNotLoanable.sql
@@ -1,6 +1,10 @@
 USE GTL
+DECLARE @StartTime datetime, @EndTime datetime
+SELECT @StartTime = GETDATE()
 SELECT I.ISBN, I.Title, I.Author, I.ItemDescription, I.Publisher, I.YearPublishing
 FROM Item AS I 
 WHERE I.ISBN NOT IN ( SELECT B.ISBN 
 					FROM Book AS B
 					WHERE B.BookType = 'Normal');
+SELECT @EndTime = GETDATE()
+SELECT DATEDIFF(ms, @StartTime, @EndTime) AS [Duration in microseconds]
